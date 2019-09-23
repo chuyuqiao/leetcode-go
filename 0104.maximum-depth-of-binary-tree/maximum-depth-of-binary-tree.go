d95e0c8620489a9c92ed5497e3883793d755209e
chuyuqiao <chuyuqiao@qutoutiao.net>
2019-09-23 23:08:58 +0800

    problem104

---
+++ 0104.maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,20 @@
+package problem104
+
+import "leetcode-go/pkg/tree"
+
+type TreeNode = tree.TreeNode
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
