5c1e9e6a86fb793116584eb911da03b2d2d7b268
chuyuqiao <yuqiao_chu@163.com>
2020-10-18 19:32:56 +0800

    2020-10-18

---
+++ 2020-10-18/threeSum.go
@@ -0,0 +1,33 @@
+package _020_10_18
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		third := n - 1
+		target := -1 * nums[first]
+		for second := first + 1; second < n; second++ {
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			if second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+
+	return ans
+}
---
+++ 2020-10-18/threeSum_test.go
@@ -0,0 +1,33 @@
+package _020_10_18
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	table := []struct {
+		input  []int
+		output [][]int
+	}{
+		//{
+		//	[]int{-1, 0, 1, 2, -1, -4},
+		//	[][]int{
+		//		{-1, -1, 2},
+		//		{-1, 0, 1},
+		//	},
+		//},
+		{
+			[]int{3, 0, -2, -1, 1, 2},
+			[][]int{
+				{-2, -1, 3},
+				{-2, 0, 2},
+				{-1, 0, 1},
+			},
+		},
+	}
+
+	for _, tbl := range table {
+		assert.Equal(t, tbl.output, threeSum(tbl.input))
+	}
+}
